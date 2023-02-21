0d6d575ac31464200d3c8efe1724771e3a055a00
Pratik-Nawale <pratiknawale03@gmail.com>
2023-02-21 10:36:15 +0530

    Level 5 - Cookies and Sessions

--- app.js
+++ app.js
@@ -4,8 +4,9 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const mongoose = require("mongoose");
-const bcrypt = require("bcrypt");
-const saltRounds = 10;
+const session = require("express-session");
+const passport = require("passport");
+const passportLocalMongoose = require("passport-local-mongoose");
 const port = 8000;
 
 const app = express();
@@ -13,11 +14,23 @@ const app = express();
 app.set("view engine", "ejs");
 
 
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
+
+app.use(session({
+    secret: "Our little secrete",
+    resave: false,
+    saveUninitialized: false
+}))
+
+app.use(passport.initialize());
+app.use(passport.session());
 
 mongoose.set('strictQuery', true);
 mongoose.connect("mongodb://localhost/userDB");
+
+// for deprication warning
+
 
 // acquire the connection (to check if it is successfull)
 const db = mongoose.connection;
@@ -26,18 +39,25 @@ const db = mongoose.connection;
 db.on("error", console.error.bind(console, "error connecting to db"));
 
 // up and running the print the message
-db.once("open", function(){
+db.once("open", function () {
     console.log("Connected to Database :: MongoDB");
 });
 
 
-const userSchema = new mongoose.Schema ({
+const userSchema = new mongoose.Schema({
     email: String,
     password: String
 });
 
+userSchema.plugin(passportLocalMongoose);
 
 const User = new mongoose.model("User", userSchema);
+
+
+passport.use(User.createStrategy());
+
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
 
 
 
@@ -53,54 +73,70 @@ app.get("/register", (req, res) => {
     return res.render("register");
 })
 
+app.get("/secrets", (req, res) => {
+    if (req.isAuthenticated()) {
+        return res.render("secrets");
+    } else {
+        res.redirect("/login");
+    }
+})
+
+app.get("/logout", function (req, res) {
+    req.logout(function (err) {
+        if (err) {
+            console.log(err);
+        }
+        res.redirect('/');
+    });
+});
+
 app.post("/register", (req, res) => {
 
-    bcrypt.hash(req.body.username, saltRounds, function(err, hash) {
-        const newUser = new User({
-            email: req.body.username,
-            password: hash
-        });
-    
-        
-        newUser.save((err) => {
-            if(err){
+    User.register(
+        { username: req.body.username },
+        req.body.password,
+        function (err, user) {
+            if (err) {
                 console.log(err);
+                res.redirect("/register");
+            } else {
+                passport.authenticate("local")(req, res, function () {
+                    res.redirect("/secrets")
+                })
             }
-            else{
-                res.render("secrets");
-            }
-        })
-    });
+        }
+    );
 
-    
+
 });
 
 
 app.post("/login", (req, res) => {
-    
-    User.findOne({email: req.body.username}, (err, foundUser) => {
-        if(err){
+
+    const user = new User({
+        username: req.body.username,
+        password: req.body.password
+    })
+
+    req.login(user, function (err) {
+        if (err) {
             console.log(err);
         } else {
-            if(foundUser){
-                bcrypt.compare(req.body.password, foundUser.password, function(err, result) {
-                    if(result === true){
-                        res.render("secrets");
-                    }
-                });
-                
-            }
+            passport.authenticate("local")(req, res, function () {
+                res.redirect("/secrets")
+            })
         }
-    });
+    })
+
 
 });
 
 
 app.listen(port, (err) => {
-    if(err){
+    if (err) {
         console.log("Error while running the server");
     }
-    else{
+    else {
         console.log("The server is up and running on the port: ", port)
     }
 })
